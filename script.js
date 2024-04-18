95cb3dfdd9cce00a032411181c31fcd27d9c4766
Fadel MENDE <fadel.mende@etu.unistra.fr>
2024-04-18 15:34:22 +0200

    fix  bug et feat on css

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function animateHeader() {
     var newText = text.substring(1) + text.substring(0, 1);
     headerTitle.innerText = newText;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var nav = document.querySelector("nav");
+    var header = document.querySelector("header");
+
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > header.offsetHeight) {
+            nav.classList.add("fixed-nav");
+        } else {
+            nav.classList.remove("fixed-nav");
+        }
+    });
+});
+
