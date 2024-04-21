b978149692e473e64d3b62662d2cfdb0b7e2094b
Fadel MENDE <fadel.mende@etu.unistra.fr>
2024-04-21 13:07:23 +0200

    feat link on website with js

--- script.js
+++ script.js
@@ -22,3 +22,29 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Sélectionnez les liens des marques de voiture
+    var francaisesLink = document.getElementById("francaises");
+    var italiennesLink = document.getElementById("italiennes");
+    var anglaisesLink = document.getElementById("anglaises");
+    var americaines= document.getElementById("americaines");
+    // Ajoutez des écouteurs d'événements de clic
+    francaisesLink.addEventListener("click", function(event) {
+        event.preventDefault(); // Empêchez le comportement de lien par défaut
+        window.location.href = "lien_vers_site_externe_francaises"; // Remplacez par l'URL du site externe pour les voitures françaises
+    });
+    italiennesLink.addEventListener("click", function(event) {
+        event.preventDefault(); // Empêchez le comportement de lien par défaut
+        window.location.href = "lien_vers_site_externe_italiennes"; // Remplacez par l'URL du site externe pour les voitures italiennes
+    });
+    anglaisesLink.addEventListener("click", function(event) {
+        event.preventDefault(); // Empêchez le comportement de lien par défaut
+        window.location.href = "lien_vers_site_externe_anglaises"; // Remplacez par l'URL du site externe pour les voitures anglaises
+    });
+    americainesLink.addEventListener("click", function(event) {
+        event.preventDefault(); // Empêchez le comportement de lien par défaut
+        window.location.href = "lien_vers_site_externe_americaines"; // Remplacez par l'URL du site externe pour les voitures anglaises
+    });
+    // Ajoutez des écouteurs d'événements de clic pour d'autres marques de voiture si nécessaire
+});
+
